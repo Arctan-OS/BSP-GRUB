c313dffae71c3a3b1d4be3fc69af6c3fe32608bc
awewsomegamer <awewsomegamer@gmail.com>
2024-07-06 21:36:37 -0700

    Added license headers

--- src/c/mm/pmm.c
+++ src/c/mm/pmm.c
@@ -1,5 +1,5 @@
 /**
- * @file freelist.c
+ * @file pmm.c
  *
  * @author awewsomegamer <awewsomegamer@gmail.com>
  *
--- src/c/multiboot/mbparse.c
+++ src/c/multiboot/mbparse.c
@@ -1,5 +1,5 @@
 /**
- * @file freelist.c
+ * @file mbparse.c
  *
  * @author awewsomegamer <awewsomegamer@gmail.com>
  *
